6e4ee824428ce61e3f69a3770f78ad6b38442a3c
Jonathan Bones <jonathan.bones@bitglow.de>
2024-12-11 15:30:15 +0100

    feat: Add combo box stories

---
+++ stories/ComboBox.stories.tsx
@@ -0,0 +1,67 @@
+import { tokens } from "@/.storybook/theme";
+import { IComboBoxOption, ComboBox as V8ComboBox } from "@fluentui/react";
+import {
+  Label,
+  makeStyles,
+  Option,
+  shorthands,
+  Text,
+  Combobox as V9ComboBox,
+} from "@fluentui/react-components";
+import { Meta } from "@storybook/react";
+import { CompareGrid } from "./CompareGrid";
+
+// Meta configuration
+const meta: Meta = {
+  title: "FluentUI",
+  component: () => <div />, // Placeholder, not used directly
+};
+export default meta;
+
+const useStyles = makeStyles({
+  error: {
+    color: tokens.colorPaletteRedForeground1,
+  },
+  comboboxError: {
+    ...shorthands.borderColor(tokens.colorPaletteRedForeground1),
+  },
+});
+
+//#region Stories
+export const ComboBox = () => {
+  const options: IComboBoxOption[] = [{ key: "77", text: "77" }];
+  const styles = useStyles();
+
+  return (
+    <CompareGrid
+      v8Component={
+        <V8ComboBox
+          label="Kundennummer"
+          options={options}
+          allowFreeform
+          errorMessage="Bitte Kundennummer eingeben"
+        />
+      }
+      v9Component={
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Label size="large" id="customer-number">
+            {"Kundennummer"}
+          </Label>
+          <V9ComboBox
+            aria-labelledby="customer-number"
+            aria-describedby="error-message"
+            className={styles.comboboxError}
+          >
+            {options.map((option) => (
+              <Option key={option.key}>{option.text}</Option>
+            ))}
+          </V9ComboBox>
+          <Text id="error-message" className={styles.error}>
+            {"Bitte Kundennummer eingeben"}
+          </Text>
+        </div>
+      }
+    />
+  );
+};
+//#endregion
