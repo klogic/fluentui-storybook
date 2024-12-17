388db373bd2272a7248c8362019ecb06c1d305af
Jonathan Bones <jonathan.bones@bitglow.de>
2024-12-17 17:44:36 +0100

    feat: Add Spinner stories

---
+++ stories/Spinner.stories.tsx
@@ -0,0 +1,25 @@
+import {
+  Spinner as V8Spinner,
+  SpinnerSize as V8SpinnerSize,
+} from "@fluentui/react";
+import { Spinner as V9Spinner } from "@fluentui/react-components";
+import { Meta } from "@storybook/react";
+import { CompareGrid } from "./CompareGrid";
+
+// Meta configuration
+const meta: Meta = {
+  title: "FluentUI",
+  component: () => <div />, // Placeholder, not used directly
+};
+export default meta;
+
+//#region Stories
+export const Spinner = () => {
+  return (
+    <CompareGrid
+      v8Component={<V8Spinner size={V8SpinnerSize.large} />}
+      v9Component={<V9Spinner />}
+    />
+  );
+};
+//#endregion
