590cec03498a5abd271fc797e39a2156e31b6c4d
Jonathan Bones <jonathan.bones@bitglow.de>
2024-12-17 11:24:34 +0100

    feat: Add Icon stories

---
+++ stories/Icon.stories.tsx
@@ -0,0 +1,50 @@
+import { tokens } from "@/.storybook/theme";
+import { Icon as V8Icon } from "@fluentui/react";
+import { makeStyles } from "@fluentui/react-components";
+import {
+  CalendarLtrRegular,
+  SearchFilled,
+  WarningRegular,
+} from "@fluentui/react-icons";
+import { Meta } from "@storybook/react";
+import { CompareGrid } from "./CompareGrid";
+
+// Meta configuration
+const meta: Meta = {
+  title: "FluentUI",
+  component: () => <div />, // Placeholder, not used directly
+};
+export default meta;
+
+const useStyles = makeStyles({
+  iconRow: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "space-evenly",
+    fontSize: tokens.fontSizeBase600,
+  },
+});
+
+//#region Stories
+export const Icon = () => {
+  const styles = useStyles();
+  return (
+    <CompareGrid
+      v8Component={
+        <div className={styles.iconRow}>
+          <V8Icon iconName="search" />
+          <V8Icon iconName="calendar" />
+          <V8Icon iconName="warning" />
+        </div>
+      }
+      v9Component={
+        <div className={styles.iconRow}>
+          <SearchFilled />
+          <CalendarLtrRegular />
+          <WarningRegular />
+        </div>
+      }
+    />
+  );
+};
+//#endregion
