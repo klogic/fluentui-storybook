d92ea09561dbd7a3225b9b600b585a7dc105b769
narongsak keawmanee <klogic@hotmail.co.th>
2025-03-04 22:38:11 +0700

    feat: adding fluent-ui with badge

---
+++ stories/Badge.tsx
@@ -0,0 +1,15 @@
+import "./Badge.module.css";
+
+import * as React from "react";
+
+import { Badge } from "@fluentui/react-components";
+import type { BadgeProps } from "@fluentui/react-components";
+
+/** Primary UI component for user interaction */
+export const BadgeComponent = ({ ...props }: BadgeProps) => {
+  return (
+    <div>
+      <Badge {...props}></Badge>
+    </div>
+  );
+};
